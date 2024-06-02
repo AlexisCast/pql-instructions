0961013cb3f31dff8630bc730b0b177d55b6a40b
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-06-02 14:04:48 -0600

    add SideBar in ErrorPage

--- src/routes/root/Error/Error.tsx
+++ src/routes/root/Error/Error.tsx
@@ -1,6 +1,7 @@
 import { useRouteError } from 'react-router-dom';
 
 import styles from './Error.module.css';
+import SideBar from '../../../component/SideBar/SideBar';
 
 type ErrorProps = {
   statusText?: string;
@@ -11,12 +12,15 @@ const Error = () => {
   const { statusText, message }: ErrorProps = useRouteError() ?? {};
 
   return (
-    <div className={styles.errorPage}>
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{statusText || message}</i>
-      </p>
+    <div className={styles.container}>
+      <SideBar />
+      <div className={styles.errorPage}>
+        <h1>Oops!</h1>
+        <p>Sorry, an unexpected error has occurred.</p>
+        <p>
+          <i>{statusText || message}</i>
+        </p>
+      </div>
     </div>
   );
 };
