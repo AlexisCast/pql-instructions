674d124e62ee2431c0d95cc984fed001c0f2701b
Alexis Castellanos <castellanos.alexis@hotmail.com>
2024-06-02 22:18:49 -0600

    update styles

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,7 +1,14 @@
+import logo from '../../assets/pql_logo.jpeg';
+
+import styles from './Home.module.css';
+
 const Home = () => {
   return (
     <div>
       <h1>Welcome to Premier Quidditch League!</h1>
+      <div className={styles.image_container}>
+        <img src={logo} className={styles.image} alt="logo" />
+      </div>
     </div>
   );
 };
